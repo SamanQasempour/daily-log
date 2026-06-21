5cf139371edbcdf682d511057ce49051b6c29fb8
SamanQasempour <samann1389@gmail.com>
2026-06-21 11:40:54 +0330

    Auto log: Sun Jun 21 11:40:54 AM +0330 2026

--- auto-log.sh
+++ auto-log.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+# تنظیم مسیرهای اصلی
 export PATH=/usr/bin:/bin:/usr/local/bin
-export GIT_SSH_COMMAND="ssh -i /home/saman/.ssh/id_ed25519 -o StrictHostKeyChecking=no"
+export GIT_SSH_COMMAND="ssh -i /home/saman/.ssh/id_ed25519 -o BatchMode=yes -o StrictHostKeyChecking=no"
+
+# رفتن به پوشه و انجام عملیات گیت
+cd /home/saman/daily-log
+/usr/bin/git add .
+/usr/bin/git commit -m "Auto log: $(/usr/bin/date)" --allow-empty
+/usr/bin/git push origin main#!/bin/bash
+export PATH=/usr/bin:/bin:/usr/local/bin
+# تنظیم کلید SSH برای این جلسه خاص
+export GIT_SSH_COMMAND="ssh -i /home/saman/.ssh/id_ed25519 -o BatchMode=yes -o StrictHostKeyChecking=no"
 
 cd /home/saman/daily-log
-
-# پاکسازی وضعیت گیت برای جلوگیری از خطا
 /usr/bin/git add .
 /usr/bin/git commit -m "Auto log: $(/usr/bin/date)" --allow-empty
 /usr/bin/git push origin main
