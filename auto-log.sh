2fe1d25b9fc139ee74fe4804e849762d717d45b7
SamanQasempour <samann1389@gmail.com>
2026-06-21 11:27:46 +0330

    Auto log: Sun Jun 21 11:27:46 AM +0330 2026

--- auto-log.sh
+++ auto-log.sh
@@ -4,13 +4,7 @@ export GIT_SSH_COMMAND="ssh -i /home/saman/.ssh/id_ed25519 -o StrictHostKeyCheck
 
 cd /home/saman/daily-log
 
-# ابتدا مطمئن شویم پوشه گیت در وضعیت سالمی است
+# پاکسازی وضعیت گیت برای جلوگیری از خطا
 /usr/bin/git add .
-# از فلگ --allow-empty استفاده می‌کنیم تا اگر تغییری نبود، اسکریپت خطا ندهد
 /usr/bin/git commit -m "Auto log: $(/usr/bin/date)" --allow-empty
-/usr/bin/git push origin main#!/bin/bash
-export PATH=/usr/bin:/bin:/usr/local/bin
-cd /home/saman/daily-log
-/usr/bin/git add .
-/usr/bin/git commit -m "Auto log: $(/usr/bin/date)"
 /usr/bin/git push origin main
